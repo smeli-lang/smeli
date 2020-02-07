4f24cc232720640b07c8df86a63cc51faba283bf
Remi Papillie <remi.papillie@gmail.com>
2020-02-07 18:28:12 +0100

    number parsing

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -1,15 +1,165 @@
-import { parseNumberLiteral, ParsingState } from "./parser";
+import { parseRegex, parseNumberLiteral, ParsingState } from "./parser";
+
+test("parseRegex: simple match", () => {
+  const state: ParsingState = { str: "aaaaa", n: 0 };
+  const re = /a+/g;
+  const found = parseRegex(state, re);
+  expect(found).toBe(true);
+  expect(state.n).toBe(5);
+});
+
+test("parseRegex: simple non-match", () => {
+  const state: ParsingState = { str: "bbbbbb", n: 0 };
+  const re = /a+/g;
+  const found = parseRegex(state, re);
+  expect(found).toBe(false);
+  expect(state.n).toBe(0);
+});
+
+test("parseRegex: substring", () => {
+  const state: ParsingState = { str: "hello world!", n: 6 };
+  const re = /[a-z]+/g;
+  const found = parseRegex(state, re);
+  expect(found).toBe(true);
+  expect(state.n).toBe(11);
+  expect(state.str.substring(6, state.n)).toBe("world");
+});
+
+test("parseRegex: multiple matches", () => {
+  const state: ParsingState = { str: "hello72world!", n: 0 };
+  const letters = /[a-z]+/g;
+  const digits = /[0-9]+/g;
+  expect(parseRegex(state, letters)).toBe(true);
+  expect(state.n).toBe(5);
+  expect(parseRegex(state, digits)).toBe(true);
+  expect(state.n).toBe(7);
+  expect(parseRegex(state, letters)).toBe(true);
+  expect(state.n).toBe(12);
+  expect(parseRegex(state, digits)).toBe(false);
+  expect(state.n).toBe(12);
+});
 
 test("parseNumberLiteral: single number", () => {
-  let state: ParsingState = { str: "125", n: 0 };
+  const state: ParsingState = { str: "125", n: 0 };
   const literal = parseNumberLiteral(state);
-  expect(literal.value).toBe(125);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(125);
   expect(state.n).toBe(3);
 });
 
-test("parseNumberLiteral: followed by operator", () => {
-  let state: ParsingState = { str: "87456+12", n: 0 };
+test("parseNumberLiteral: negative number", () => {
+  const state: ParsingState = { str: "-125", n: 0 };
   const literal = parseNumberLiteral(state);
-  expect(literal.value).toBe(87456);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(-125);
+  expect(state.n).toBe(4);
+});
+
+test("parseNumberLiteral: decimal number", () => {
+  const state: ParsingState = { str: "0.4", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(0.4);
+  expect(state.n).toBe(3);
+});
+
+test("parseNumberLiteral: decimal-only number", () => {
+  const state: ParsingState = { str: ".2", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(0.2);
+  expect(state.n).toBe(2);
+});
+
+test("parseNumberLiteral: negative decimal-only number", () => {
+  const state: ParsingState = { str: "-.2", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(-.2);
+  expect(state.n).toBe(3);
+});
+
+test("parseNumberLiteral: binary", () => {
+  const state: ParsingState = { str: "0b101010", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(42);
+  expect(state.n).toBe(8);
+});
+
+test("parseNumberLiteral: binary (negative)", () => {
+  const state: ParsingState = { str: "-0b101010", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(-42);
+  expect(state.n).toBe(9);
+});
+
+test("parseNumberLiteral: binary (invalid)", () => {
+  const state: ParsingState = { str: "0b141090", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).toBeNull();
+  expect(state.n).toBe(0);
+});
+
+test("parseNumberLiteral: octal", () => {
+  const state: ParsingState = { str: "0o123", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(83);
   expect(state.n).toBe(5);
+});
+
+test("parseNumberLiteral: octal (negative)", () => {
+  const state: ParsingState = { str: "-0o123", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(-83);
+  expect(state.n).toBe(6);
+});
+
+test("parseNumberLiteral: octal (invalid)", () => {
+  const state: ParsingState = { str: "0o749", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).toBeNull();
+  expect(state.n).toBe(0);
+});
+
+test("parseNumberLiteral: hex", () => {
+  const state: ParsingState = { str: "0x32", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(50);
+  expect(state.n).toBe(4);
+});
+
+test("parseNumberLiteral: hex (negative)", () => {
+  const state: ParsingState = { str: "-0x32", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(-50);
+  expect(state.n).toBe(5);
+});
+
+test("parseNumberLiteral: hex (invalid)", () => {
+  const state: ParsingState = { str: "0xabfgz", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).toBeNull();
+  expect(state.n).toBe(0);
+});
+
+test("parseNumberLiteral: followed by operator", () => {
+  const state: ParsingState = { str: "87456+12", n: 0 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(87456);
+  expect(state.n).toBe(5);
+});
+
+test("parseNumberLiteral: inside substring", () => {
+  const state: ParsingState = { str: "123 + 0xff + 987", n: 6 };
+  const literal = parseNumberLiteral(state);
+  expect(literal).not.toBeNull();
+  expect(literal!.value).toBe(255);
+  expect(state.n).toBe(10);
 });
--- src/parser.ts
+++ src/parser.ts
@@ -1,9 +1,28 @@
 import { NumberLiteral } from "./ast";
 
+// terminals
+const lineEnd = /^(\r\n|\r|\n)/g;
+const whitespace = /^[ \t]*/g;
+const number = /\b-?(([1-9]+[0-9]*)|([0-9]*\.[0-9]+)|(0b[01]+)|(0o[0-7]+)|(0x[0-9a-fA-F]+))\b/g;
+const identifier = /^[_a-zA-Z][_0-9a-zA-Z]*/g;
+
 export type ParsingState = {
   str: string;
   n: number;
 };
+
+export function parseRegex(state: ParsingState, regex: RegExp) {
+  // setup parser to read from the current string location
+  regex.lastIndex = state.n;
+
+  const found = regex.test(state.str);
+  if (found) {
+    // update the position only when found
+    state.n = regex.lastIndex;
+  }
+
+  return found;
+}
 
 export function skipWhitespace(state: ParsingState) {
   while (state.n < state.str.length && /[ \t\r\n]/.test(state.str[state.n])) {
@@ -12,14 +31,21 @@ export function skipWhitespace(state: ParsingState) {
 }
 
 export function parseNumberLiteral(state: ParsingState) {
-  const start = state.n;
-  while (
-    state.n < state.str.length &&
-    /[0123456789]/.test(state.str[state.n])
-  ) {
-    state.n++;
+  let start = state.n;
+  if (!parseRegex(state, number)) {
+    return null;
   }
 
-  const value = Number(state.str.substring(start, state.n));
-  return new NumberLiteral(value);
+  // skip the minus sign before parsing with the Number constructor
+  // (some cases don't work with a string as input, such as negative binary)
+  let negative = false;
+  if (state.str[start] === '-') {
+    negative = true;
+    start++;
+  }
+
+  const stringValue = state.str.substring(start, state.n);
+  const value = Number(stringValue);
+
+  return new NumberLiteral(negative ? -value : value);
 }
