356d513733e3d31b1b2647a00d2c43b123de6477
Remi Papillie <remi.papillie@gmail.com>
2020-02-14 06:23:59 +0100

    better engine test

--- src/engine.spec.ts
+++ src/engine.spec.ts
@@ -1,7 +1,13 @@
 import Engine from "./engine";
 
-test("should parse a basic statement", () => {
+test("should run a basic statement", () => {
   const engine = new Engine("a = 42");
   expect(engine.rootStatements.length).toBe(1);
   expect(engine.allStatements.length).toBe(1);
+
+  engine.step(1);
+  expect(engine.globalScope.lookup("a")?.evaluate().value).toBe(42);
+
+  engine.step(-1);
+  expect(engine.globalScope.lookup("a")).toBeNull();
 });
