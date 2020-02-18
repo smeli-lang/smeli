e2c7d55b3f9d1573b33218ddadb4851fe0312af0
Remi Papillie <remi.papillie@gmail.com>
2020-02-18 14:09:47 +0100

    iterating on type system design

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,18 @@
 export type TypeTraits = {
   // typed scope constructor
-  "constructor"?: Function;
+  constructor?: Function;
 
-  // 
+  //
   //"call"?: Function;
 
   // binary operators
   "operator_+"?: (lhs: any, rhs: any) => any;
   "operator_-"?: (lhs: any, rhs: any) => any;
-}
+};
 
 export type TypeDefinition = {
   traits: TypeTraits;
-}
+};
 
 export interface ScopeType {
   define(): any;
@@ -22,14 +22,36 @@ export interface ScopeType {
 const numberType: TypeDefinition = {
   traits: {
     "operator_+": (lhs: number, rhs: number) => lhs + rhs,
-    "operator_-": (lhs: number, rhs: number) => lhs - rhs,
+    "operator_-": (lhs: number, rhs: number) => lhs - rhs
+  }
+};
+
+export type TypedValue = {
+  type: TypeDefinition;
+  value: any;
+};
+
+export interface Number extends TypedValue {}
+
+class Vec2 implements ScopeType {
+  values: [number, number];
+
+  constructor() {
+    this.values = [0, 0];
+  }
+
+  define() {
+    return {
+      bindings: {
+        x: 0,
+        y: 0
+      }
+    };
   }
 }
 
-Object.defineProperty(Number.prototype, "__smeli_type__", numberType);
-
-export type TypedValue = {
-  "__smeli_type__": TypeDefinition;
-}
-
-export interface Number extends TypedValue {}
+const vec2Type: TypeDefinition = {
+  traits: {
+    constructor: Vec2
+  }
+};
