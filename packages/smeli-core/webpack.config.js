e142cda13272ee1e6ec26dad9d969a31342662d7
Remi Papillie <remi.papillie@gmail.com>
2020-02-28 13:08:23 +0100

    improved packaging of @smeli/core

--- packages/smeli-core/webpack.config.js
+++ packages/smeli-core/webpack.config.js
@@ -19,7 +19,25 @@ module.exports = {
   output: {
     library: "smeli",
     libraryTarget: "umd",
-    filename: `smeli.min.js`,
+    filename: `smeli-core.min.js`,
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [new DtsBundlePlugin()]
+};
+
+// Thanks Vladimir :)
+// https://medium.com/@vladimirtolstikov/how-to-merge-d-ts-typings-with-dts-bundle-and-webpack-e8903d699576
+function DtsBundlePlugin() {}
+DtsBundlePlugin.prototype.apply = function(compiler) {
+  compiler.plugin("done", function() {
+    var dts = require("dts-bundle");
+
+    dts.bundle({
+      name: "smeli",
+      main: "dist/index.d.ts",
+      out: "smeli-core.min.d.ts",
+      removeSource: true,
+      outputAsModuleFolder: true // to use npm in-package typings
+    });
+  });
 };
