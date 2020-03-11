9ec155cb8957b215b5d916237cf914ccfb34282f
Remi Papillie <remi.papillie@gmail.com>
2020-03-11 18:09:22 +0100

    wip dom slider

--- packages/smeli-plugin-dom/src/slider.ts
+++ packages/smeli-plugin-dom/src/slider.ts
@@ -1,5 +1,5 @@
 import { Scope, TypedValue, TypeDefinition, TypeTraits } from "@smeli/core";
-import template from "./plop.pug";
+import template from "./slider.pug";
 
 interface DomNode extends TypedValue {
   node: HTMLElement;
@@ -11,15 +11,29 @@ class Slider implements DomNode {
 
   constructor(scope: Scope) {
     this.node = document.createElement("div");
-    (document.querySelector("#preview") as HTMLElement).innerHTML = template({
-      name: "LOL"
-    });
     this.scope = scope;
 
     this.scope.bind("value", 42);
+    this.update({ value: 42 });
   }
 
-  unbind() {}
+  update(evaluatedScope: any) {
+    // render with the updated values
+    this.node.innerHTML = template(evaluatedScope);
+
+    const slider = this.node.querySelector(".slider") as HTMLInputElement;
+    slider.addEventListener("input", () => {
+      this.scope.bind("value", parseInt(slider.value));
+    });
+
+    const root = document.querySelector("#preview") as HTMLElement;
+    root.appendChild(this.node);
+  }
+
+  unbind() {
+    // destroy the whole subtree and the associated callbacks
+    this.node.innerHTML = "";
+  }
 
   type() {
     return SliderType;
@@ -28,6 +42,8 @@ class Slider implements DomNode {
 
 const SliderType: TypeTraits = {
   __name__: () => "slider",
+  __str__: (self: Slider) =>
+    `slider(${(self.node.querySelector(".slider") as HTMLInputElement).value})`,
 
   __bind__: (scope: Scope) => new Slider(scope),
   __unbind__: (self: Slider) => self.unbind(),
