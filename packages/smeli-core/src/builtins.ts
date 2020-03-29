c81b044e7063bc9e2dcc5cb00b12a068d5512f99
Remi Papillie <remi.papillie@gmail.com>
2020-03-29 18:29:20 +0200

    wip simple builtin timer

--- packages/smeli-core/src/builtins.ts
+++ packages/smeli-core/src/builtins.ts
@@ -5,7 +5,7 @@ import {
   NumberValue,
   FunctionValue
 } from "./types";
-import { Binding } from "./scope";
+import { Binding, Scope } from "./scope";
 
 function max(args: TypedValue[]): TypedValue {
   const result = Math.max(
@@ -17,9 +17,45 @@ function max(args: TypedValue[]): TypedValue {
   return new NumberValue(result);
 }
 
+const timer = {
+  name: "timer",
+  evaluate: (parentScope: Scope) => {
+    const scope = new Scope(parentScope);
+    scope.push([
+      {
+        name: "time",
+        evaluate: () => new NumberValue(0)
+      },
+      {
+        name: "#update",
+        evaluate: scope => {
+          let binding: Binding | null = null;
+          setInterval(() => {
+            if (binding) {
+              scope.pop(binding);
+              binding = null;
+            }
+
+            binding = {
+              name: "time",
+              evaluate: () => new NumberValue(Date.now())
+            };
+            scope.push(binding);
+          }, 10);
+
+          return new NumberValue(0);
+        }
+      }
+    ]);
+
+    return scope;
+  }
+};
+
 export const builtins: Binding[] = [
   {
     name: "max",
     evaluate: () => new FunctionValue(max)
-  }
+  },
+  timer
 ];
