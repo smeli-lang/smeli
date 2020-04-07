70cf56503fdc65bf96e4e4c3c8181781366a3588
Remi Papillie <remi.papillie@gmail.com>
2020-04-07 14:10:38 +0200

    extracted the cli entry point into a separate file

--- packages/smeli-cli/src/index.ts
+++ packages/smeli-cli/src/index.ts
@@ -1,24 +1,27 @@
-#!/usr/bin/env node
 import webpack from "webpack";
 import path from "path";
 
-const template = require("@smeli/template-slides");
+const run = () => {
+  const template = require("@smeli/template-slides");
 
-const templateOptions = {
-  rootFile: path.resolve("index.smeli"),
-  outputPath: path.resolve("dist")
+  const templateOptions = {
+    rootFile: path.resolve("index.smeli"),
+    outputPath: path.resolve("dist")
+  };
+
+  const webpackConfig = template(templateOptions);
+
+  const compiler = webpack(webpackConfig);
+
+  compiler.run((err, stats) => {
+    if (err || stats.hasErrors()) {
+      console.log("failed!");
+      console.log(err);
+      console.log(stats);
+    } else {
+      console.log("output written to", templateOptions.outputPath);
+    }
+  });
 };
 
-const webpackConfig = template(templateOptions);
-
-const compiler = webpack(webpackConfig);
-
-compiler.run((err, stats) => {
-  if (err || stats.hasErrors()) {
-    console.log("failed!");
-    console.log(err);
-    console.log(stats);
-  } else {
-    console.log("output written to", templateOptions.outputPath);
-  }
-});
+export { run };
