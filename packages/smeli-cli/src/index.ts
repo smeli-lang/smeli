4b18f1232b5495bc6497826ed378531a86d5b2d9
Remi Papillie <remi.papillie@gmail.com>
2020-04-07 18:27:10 +0200

    minimal watch option

--- packages/smeli-cli/src/index.ts
+++ packages/smeli-cli/src/index.ts
@@ -13,7 +13,7 @@ const run = () => {
 
   const compiler = webpack(webpackConfig);
 
-  compiler.run((err, stats) => {
+  const handler: webpack.ICompiler.Handler = (err, stats) => {
     if (err || stats.hasErrors()) {
       console.log("failed!");
       console.log(err);
@@ -21,7 +21,15 @@ const run = () => {
     } else {
       console.log("output written to", templateOptions.outputPath);
     }
-  });
+  };
+
+  // this will be replaced by a proper argument parsing library
+  // when implemented
+  if (process.argv.length > 2 && process.argv[2] === "-w") {
+    compiler.watch({}, handler);
+  } else {
+    compiler.run(handler);
+  }
 };
 
 export { run };
