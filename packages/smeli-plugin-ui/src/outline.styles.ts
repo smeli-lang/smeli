f83ed9be432e2fe9d9f420c8fc449e73ab898180
Remi Papillie <remi.papillie@gmail.com>
2020-04-14 15:26:03 +0200

    minor comma fix

--- packages/smeli-plugin-ui/src/outline.styles.ts
+++ packages/smeli-plugin-ui/src/outline.styles.ts
@@ -5,28 +5,29 @@ export const outlineStyles = (theme: Theme) => ({
   "border-color": theme.colors.primary,
   padding: "32px",
   flex: 1,
+  overflow: "hidden",
   "& h1": {
     margin: "0px",
     "margin-top": "32px",
-    padding: "0px"
+    padding: "0px",
   },
   "& h2,h3,h4,h5,h6": {
     margin: "16px",
-    padding: "0px"
+    padding: "0px",
   },
   "& h2": {
-    "margin-left": "32px"
+    "margin-left": "32px",
   },
   "& h3": {
-    "margin-left": "64px"
+    "margin-left": "64px",
   },
   "& h4": {
-    "margin-left": "96px"
+    "margin-left": "96px",
   },
   "& h5": {
-    "margin-left": "128px"
+    "margin-left": "128px",
   },
   "& h6": {
-    "margin-left": "160px"
-  }
+    "margin-left": "160px",
+  },
 });
