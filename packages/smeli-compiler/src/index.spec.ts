86f6765312ba93e8453d09ca62d66752a25693c7
Remi Papillie <remi.papillie@gmail.com>
2020-05-13 15:05:14 +0200

    added plugin compiler directive

--- packages/smeli-compiler/src/index.spec.ts
+++ packages/smeli-compiler/src/index.spec.ts
@@ -10,3 +10,24 @@ test("plugin list", () => {
 
   expect(result.plugins).toEqual(["@smeli/plugin-ui", "hello"]);
 });
+
+test("plugin duplicates", () => {
+  const result = compile({
+    mainChunk: `
+      @plugin("hello")
+      @plugin("hello")
+    `,
+  });
+
+  expect(result.plugins).toEqual(["hello"]);
+});
+
+test("unknown directive", () => {
+  expect(() =>
+    compile({
+      mainChunk: `
+      @dinosaur("meteorite")
+    `,
+    })
+  ).toThrowError("Unknown compiler directive: dinosaur");
+});
--- packages/smeli-compiler/src/index.ts
+++ packages/smeli-compiler/src/index.ts
@@ -1,13 +1,58 @@
+const compilerDirectiveRegex = /@([A-Za-z_][A-Za-z0-9_]*)\("([^"]*)"\)/g;
+
+function executeDirective(
+  name: string,
+  parameter: string,
+  plugins: string[]
+): string {
+  switch (name) {
+    case "plugin": {
+      // accumulate the plugin in the shared list
+      if (plugins.indexOf(parameter) === -1) {
+        plugins.push(parameter);
+      }
+
+      // remove the plugin directive from the runtime code
+      return "";
+    }
+
+    case "inline": {
+      // replace the directive with inlined chunk
+      return "";
+    }
+  }
+
+  throw new Error("Unknown compiler directive: " + name);
+}
+
+function compileChunk(code: string, plugins: string[]) {
+  return code.replace(
+    compilerDirectiveRegex,
+    (match: string, name: string, parameter: string) => {
+      return executeDirective(name, parameter, plugins);
+    }
+  );
+}
+
 export type CompileOptions = {
-  mainChunk: string;
+  mainChunk?: string;
+  resolveChunk?: (filename: string) => string;
 };
 
 export type CompileResult = {
   plugins: string[];
+  fullCode: string;
 };
 
-export function compile(options: CompileOptions): CompileResult {
+export function compile({
+  mainChunk = "",
+  resolveChunk = () => "",
+}: CompileOptions = {}): CompileResult {
+  const plugins: string[] = [];
+  const fullCode = compileChunk(mainChunk, plugins);
+
   return {
-    plugins: [],
+    plugins,
+    fullCode,
   };
 }
