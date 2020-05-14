0288030e2af7206199e8099aa7df0642361d0ceb
Remi Papillie <remi.papillie@gmail.com>
2020-05-14 23:45:25 +0200

    editable code editor

--- packages/smeli-plugin-ace/src/editor.ts
+++ packages/smeli-plugin-ace/src/editor.ts
@@ -3,7 +3,7 @@ import ace from "ace-builds";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/mode-python";
 
-import { Scope, StringValue, StringType } from "@smeli/core";
+import { Binding, Scope, StringValue, StringType } from "@smeli/core";
 import { DomNode } from "@smeli/plugin-ui";
 
 import { evaluateAceStyles } from "./styles";
@@ -22,18 +22,47 @@ export const editor = {
         evaluate: (scope: Scope) => {
           const aceStyles = evaluateAceStyles(scope);
 
-          const code = scope.evaluate("code", StringType) as StringValue;
-
           const element = document.createElement("pre");
           element.className = aceStyles.editor;
 
-          ace.edit(element, {
-            value: code.value,
+          const editor = ace.edit(element, {
             theme: "ace/theme/monokai",
             mode: "ace/mode/python",
           });
 
-          return new DomNode(element);
+          let override: Binding | null = null;
+          editor.session.on("change" as any, () => {
+            const code = editor.session.getValue();
+
+            // make sure the override is bound outside the engine update
+            setTimeout(() => {
+              if (override) {
+                scope.pop(override);
+              }
+
+              override = {
+                name: "code",
+                evaluate: () => new StringValue(code),
+              };
+
+              scope.push(override);
+            }, 0);
+          });
+
+          const result = scope.evaluate(() => new DomNode(element));
+
+          // cache editor
+          return (scope: Scope) => {
+            const code = scope.evaluate("code", StringType) as StringValue;
+
+            // guard against infinite recursion
+            if (!override) {
+              editor.session.setValue(code.value);
+              editor.selection.clearSelection();
+            }
+
+            return result;
+          };
         },
       },
     ]);
