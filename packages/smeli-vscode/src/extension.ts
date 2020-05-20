1b56ba9222dbca186b23db18e8e14e0a362a8ef4
Remi Papillie <remi.papillie@gmail.com>
2020-05-20 04:40:37 +0200

    first (hacky) preview rendering

--- packages/smeli-vscode/src/extension.ts
+++ packages/smeli-vscode/src/extension.ts
@@ -1,3 +1,5 @@
+import * as child_process from "child_process";
+import * as path from "path";
 import * as vscode from "vscode";
 
 class Preview extends vscode.Disposable {
@@ -14,6 +16,8 @@ class Preview extends vscode.Disposable {
 
     this.document = document;
 
+    const folder = path.dirname(document.uri.fsPath);
+
     const title = "Smeli: " + document.fileName;
     this.panel = vscode.window.createWebviewPanel(
       "smeli",
@@ -21,6 +25,7 @@ class Preview extends vscode.Disposable {
       vscode.ViewColumn.Beside,
       {
         enableScripts: true,
+        localResourceRoots: [vscode.Uri.file(path.join(folder, "dist"))],
       }
     );
 
@@ -30,7 +35,30 @@ class Preview extends vscode.Disposable {
     let code = document.getText();
 
     const webview = this.panel.webview;
-    webview.html = "<pre>" + code + "</pre>";
+    webview.html = "<p>Compiling " + document.uri.fsPath + "</p>";
+
+    child_process.exec(
+      "yarn smeli",
+      { cwd: folder },
+      (error, stdout, stderr) => {
+        /*webview.html = `
+          <h1>Compile result for ${document.uri.fsPath}</h1>
+          <h2>Output</h2>
+          <p>${stdout}</p>
+          <h2>Errors</h2>
+          <p>${stderr}</p>
+          <p>${error}</p>
+        `;*/
+
+        const indexFile = path.join(folder, "dist", "index.html");
+        vscode.workspace.fs
+          .readFile(vscode.Uri.file(indexFile))
+          .then((buffer) => {
+            const htmlContent = Buffer.from(buffer).toString("utf8");
+            webview.html = htmlContent;
+          });
+      }
+    );
 
     this.changeEvent = vscode.workspace.onDidChangeTextDocument((event) => {
       if (event.document === this.document) {
@@ -41,7 +69,7 @@ class Preview extends vscode.Disposable {
             change.text +
             code.substring(change.rangeOffset + change.rangeLength);
 
-          webview.html = "<pre>" + code + "</pre>";
+          //webview.html = "<pre>" + code + "</pre>";
         });
       }
     });
