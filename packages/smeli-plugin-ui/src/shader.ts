340603fa6359f10f1f66baaa6877933152416dba
Remi Papillie <remi.papillie@gmail.com>
2020-08-05 13:21:28 +0200

    cleaner GL context

--- packages/smeli-plugin-ui/src/shader.ts
+++ packages/smeli-plugin-ui/src/shader.ts
@@ -35,48 +35,119 @@ const defaultFragmentShaderCode = `
   }
 `;
 
-function compileShader(gl: WebGLRenderingContext, type: number, code: string) {
-  const shader = gl.createShader(type) as WebGLShader;
+class GLDrawContext extends TypedValue {
+  static typeName = "gl_draw_context";
 
-  gl.shaderSource(shader, code);
-  gl.compileShader(shader);
+  canvas: HTMLCanvasElement;
+  gl: WebGLRenderingContext;
 
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    const error = gl.getShaderInfoLog(shader);
-    throw new Error(`Failed to compile shader: ${error}`);
+  program: WebGLProgram | null = null;
+  vbo: WebGLBuffer | null = null;
+
+  disposed: boolean = false;
+
+  constructor() {
+    super();
+
+    this.canvas = document.createElement("canvas");
+
+    this.gl = this.canvas.getContext("webgl", {
+      premultipliedAlpha: false,
+    }) as WebGLRenderingContext;
   }
 
-  return shader;
-}
+  loadShader(type: number, code: string) {
+    if (this.program === null) {
+      throw new Error(
+        "Internal error: shader program should exist at this point"
+      );
+    }
 
-function compileProgram(
-  gl: WebGLRenderingContext,
-  vertexCode: string,
-  fragmentCode: string
-) {
-  const vertexShader = compileShader(gl, gl.VERTEX_SHADER, vertexCode);
-  const fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fragmentCode);
+    const shader = this.gl.createShader(type) as WebGLShader;
 
-  const program = gl.createProgram() as WebGLProgram;
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
+    this.gl.shaderSource(shader, code);
+    this.gl.compileShader(shader);
 
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    const error = gl.getProgramInfoLog(program);
-    throw new Error(`Shader link failed: ${error}`);
+    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      const error = this.gl.getShaderInfoLog(shader);
+      this.gl.deleteShader(shader);
+      throw new Error(`Failed to compile shader: ${error}`);
+    }
+
+    this.gl.attachShader(this.program, shader);
+
+    // tag the shader for deletion when the program goes away
+    // (it will remain live as long as it is attached)
+    this.gl.deleteShader(shader);
+
+    return shader;
   }
 
-  return program;
+  loadProgram(vertexCode: string, fragmentCode: string) {
+    // unload previously loaded program
+    if (this.program !== null) {
+      this.gl.deleteProgram(this.program);
+      this.program = null;
+    }
+
+    this.program = this.gl.createProgram() as WebGLProgram;
+
+    this.loadShader(this.gl.VERTEX_SHADER, vertexCode);
+    this.loadShader(this.gl.FRAGMENT_SHADER, fragmentCode);
+
+    this.gl.linkProgram(this.program);
+
+    if (!this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)) {
+      const error = this.gl.getProgramInfoLog(this.program);
+      this.gl.deleteProgram(this.program);
+      throw new Error(`Shader link failed: ${error}`);
+    }
+
+    this.gl.bindAttribLocation(this.program, 0, "position");
+  }
+
+  loadGeometry(data: number[]) {
+    if (this.vbo !== null) {
+      this.gl.deleteBuffer(this.vbo);
+      this.vbo = null;
+    }
+
+    this.vbo = this.gl.createBuffer() as WebGLBuffer;
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array(data),
+      this.gl.STATIC_DRAW
+    );
+  }
+
+  dispose() {
+    if (this.program) {
+      this.gl.deleteProgram(this.program);
+      this.program = null;
+    }
+
+    if (this.vbo !== null) {
+      this.gl.deleteBuffer(this.vbo);
+      this.vbo = null;
+    }
+
+    this.disposed = true;
+  }
 }
 
 function redraw(
-  canvas: HTMLCanvasElement,
-  gl: WebGLRenderingContext,
-  vbo: WebGLBuffer,
-  program: WebGLProgram,
+  context: GLDrawContext,
   uniforms: { [key: string]: TypedValue }
 ) {
+  // this function is one frame late
+  if (context.disposed) {
+    return;
+  }
+
+  const canvas = context.canvas;
+  const gl = context.gl;
+
   const container = canvas.parentElement as HTMLElement;
   const width = container.clientWidth;
   const height = container.clientHeight;
@@ -97,10 +168,13 @@ function redraw(
 
   gl.viewport(0, 0, width, height);
 
-  gl.useProgram(program);
+  gl.useProgram(context.program);
 
   for (const [name, value] of Object.entries(uniforms)) {
-    const location = gl.getUniformLocation(program, name);
+    const location = gl.getUniformLocation(
+      context.program as WebGLProgram,
+      name
+    );
     if (location !== null) {
       if (value.is(NumberValue)) {
         gl.uniform1f(location, value.value);
@@ -112,7 +186,7 @@ function redraw(
     }
   }
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bindBuffer(gl.ARRAY_BUFFER, context.vbo);
   gl.enableVertexAttribArray(0);
   gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
 
@@ -129,49 +203,44 @@ export const shader = {
         evaluate: (scope: Scope) => new StringValue(defaultFragmentShaderCode),
       },
       {
+        name: "#gl:context",
+        evaluate: (scope: Scope) => new GLDrawContext(),
+      },
+      {
         name: "#ui:node",
         evaluate: (scope: Scope) => {
           const styles = evaluateUiStyles(scope);
-          const code = scope.evaluate("code").as(StringValue);
 
           const container = document.createElement("div");
           container.className = "container " + styles.shader;
 
-          const canvas = document.createElement("canvas");
-          container.appendChild(canvas);
-
-          const gl = canvas.getContext("webgl", {
-            premultipliedAlpha: false,
-          }) as WebGLRenderingContext;
+          const context = scope.evaluate("#gl:context").as(GLDrawContext);
+          container.appendChild(context.canvas);
 
           const result = scope.evaluate(() => new DomNode(container));
 
-          const program = compileProgram(gl, vertexShaderCode, code.value);
-
           const quadVertices = [-1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0];
-          const vbo = gl.createBuffer() as WebGLBuffer;
-          gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-          gl.bufferData(
-            gl.ARRAY_BUFFER,
-            new Float32Array(quadVertices),
-            gl.STATIC_DRAW
-          );
+          context.loadGeometry(quadVertices);
 
           const startTime = new NumberValue(Date.now() * 0.001);
 
-          // cache DOM element
+          // cache DOM element and GL context
           return (scope: Scope) => {
-            const time = scope
-              .evaluate("time")
-              .as(NumberValue)
-              .__sub__(startTime);
+            const code = scope.evaluate("code").as(StringValue);
+            context.loadProgram(vertexShaderCode, code.value);
 
-            // redraw next frame (ensures correct layout, aspect ratio, etc.)
-            requestAnimationFrame(() =>
-              redraw(canvas, gl, vbo, program, { time })
-            );
+            // cache compiled code
+            return (scope: Scope) => {
+              const time = scope
+                .evaluate("time")
+                .as(NumberValue)
+                .__sub__(startTime);
 
-            return result;
+              // redraw next frame (ensures correct layout, aspect ratio, etc.)
+              requestAnimationFrame(() => redraw(context, { time }));
+
+              return result;
+            };
           };
         },
       },
