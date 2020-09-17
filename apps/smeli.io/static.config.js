cd9b5562b3cd913b5a05b53b161fa294be6ca04a
Remi Papillie <remi.papillie@gmail.com>
2020-09-17 16:39:00 +0200

    removed REST call from website build

--- apps/smeli.io/static.config.js
+++ apps/smeli.io/static.config.js
@@ -1,40 +1,47 @@
-import axios from 'axios'
-import path from 'path'
+import axios from "axios";
+import path from "path";
 // import { Post } from './types'
 
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
-  entry: path.join(__dirname, 'src', 'index.tsx'),
+  entry: path.join(__dirname, "src", "index.tsx"),
   getRoutes: async () => {
-    const { data: posts } /* :{ data: Post[] } */ = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+    const { data: posts } /* :{ data: Post[] } */ = {
+      data: [
+        {
+          userId: 1,
+          id: 1,
+          title: "Welcome to Smeli!",
+          body: "Hello World!",
+        },
+      ],
+    };
     return [
       {
-        path: '/blog',
+        path: "/blog",
         getData: () => ({
           posts,
         }),
         children: posts.map((post /* : Post */) => ({
           path: `/post/${post.id}`,
-          template: 'src/containers/Post',
+          template: "src/containers/Post",
           getData: () => ({
             post,
           }),
         })),
       },
-    ]
+    ];
   },
   plugins: [
-    'react-static-plugin-typescript',
+    "react-static-plugin-typescript",
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
+        location: path.resolve("./src/pages"),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap"),
   ],
-}
+};
